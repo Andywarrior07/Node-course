0ae0659208efffb227405013fc4672d56a069011
andresguerrero07 <andres@legalbot.cl>
2021-02-15 10:40:40 -0300

    add create and list tasks

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 require('colors');
 
-const { inquirerMenu, pause } = require('./helpers/inquirer');
-
-console.clear();
+const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
+const Tasks = require('./models/tasks');
 
 const main = async () => {
-  console.log('hola mundo');
-
   let opt = '';
+  const tasks = new Tasks();
 
   do {
     opt = await inquirerMenu();
+
+    switch (opt) {
+      case '1':
+        const description = await readInput('Descripcion: ');
+        tasks.createTask(description);
+        break;
+      case '2':
+        console.log(tasks._list);
+        break;
+      case '3':
+        break;
+    }
 
     await pause();
   } while (opt !== '0');
--- models/tasks.js
+++ models/tasks.js
@@ -1,6 +1,14 @@
+const Task = require('./task');
+
 class Tasks {
   constructor() {
     this._list = {};
+  }
+
+  createTask(description) {
+    const task = new Task(description);
+
+    this._list[task.id] = task;
   }
 }
 
